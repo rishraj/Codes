a84ca01a1acec4e174906a9bd31cacd8554c4fc2
Rishav Raj <rishav.raj2309@gmail.com>
2024-07-14 18:13:24 +0530

    Day 9: DS practise

---
+++ 022. Partition List (86).py
@@ -0,0 +1,15 @@
+def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+    cur = head
+    l1_head, l2_head = ListNode(-1), ListNode(-1)
+    l1_tail, l2_tail = l1_head, l2_head
+    while cur:
+        if cur.val < x:
+            l1_tail.next = cur
+            l1_tail = l1_tail.next
+        else:
+            l2_tail.next = cur
+            l2_tail = l2_tail.next
+        cur = cur.next
+    l1_tail.next = l2_head.next
+    l2_tail.next = None
+    return l1_head.next
