418f46f67396ea66402eda48abf29cb518e50dd3
Rishav Raj <rishav.raj2309@gmail.com>
2024-07-16 18:39:07 +0530

    Day 11: DS practise

---
+++ 026. Add Two Numbers (2).py
@@ -0,0 +1,46 @@
+def addTwoNumbers (self, 11: Optional[ListNode], 12: Optional[ListNode])-> Optional [ListNode]:
+    # soln works, but is not the most elegant
+    """ dhead, c = ListNode(-1), 0
+    cur = dhead
+    while l1 or l2 or c:
+        if l1 and l2:
+            s = l1.val + l2.val + c
+            cur.next = ListNode(s % 10)
+            cur = cur.next
+            l1 = l1.next 
+            l2 = l2.next
+            c = s // 10
+        elif l1:
+            s = l1.val + c
+            cur.next = ListNode(s % 10)
+            cur = cur.next
+            l1 = l1.next 
+            c = s // 10
+        elif l2:
+            s = l2.val + c
+            cur.next = ListNode(s % 10)
+            cur = cur.next
+            l2 = l2.next 
+            c = s // 10
+        elif c:
+            cur.next = ListNode(c)
+    return dhead.next """ 
+    
+    # pythonic way
+    dhead = ListNode(-1)
+    cur = dhead
+    c = 0
+    while l1 or l2 or c:
+        l1_val = l1.val if l1 else 0
+        l2_val = l2.val if l2 else 0
+
+        s = l1_val + l2_val + c
+        cur.next = ListNode(s % 10)
+        cur = cur.next
+        c = s // 10
+
+
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+
+    return dhead.next
