e413a6a8545c03ddf1ebe48a6aea311a737277d0
Rishav Raj <rishav.raj2309@gmail.com>
2024-07-22 22:53:33 +0530

    Day 17: DS practise

--- 037. Binary Tree Preorder Traversal (144).py
+++ 037. Binary Tree Preorder Traversal (144).py
@@ -1,9 +1,20 @@
 def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-    res = [] # since list is mutable, self.res initialisation not needed
+    # revursive soln.
+    """ res = [] # since list is mutable, self.res initialisation not needed
     def helper(node):
         if not node: return
         res.append(node.val)
         helper(node.left)
         helper(node.right)
     helper(root)
+    return res """
+    
+    # iterative soln.
+    if not root: return root # edge case
+    res, stack = [], [root]
+    while stack: # here root will never be null (thus, "while root or stack:" not needed)
+        node = stack.pop()
+        res.append(node.val)
+        if node.right: stack.append(node.right) # right added before left so that it's popped later
+        if node.left: stack.append(node.left)
     return res
