d6e021ad223ab6ac67ef66274b39d390eeb0b885
Rishav Raj <rishav.raj2309@gmail.com>
2024-07-22 20:18:47 +0530

    Day 17: DS practise

---
+++ 038. Binary Tree Postorder Traversal (145).py
@@ -0,0 +1,9 @@
+def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    res = []
+    def helper(node):
+        if not node: return
+        helper(node.left)
+        helper(node.right)
+        res.append(node.val)
+    helper(root)
+    return res
