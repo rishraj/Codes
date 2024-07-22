c00084c3b4b184792a892c950c599d7ee95fde8c
Rishav Raj <rishav.raj2309@gmail.com>
2024-07-22 22:35:12 +0530

    Day 17: DS practise

--- 039. Binary Tree Inorder Traversal (94).py
+++ 039. Binary Tree Inorder Traversal (94).py
@@ -1,9 +1,22 @@
 def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-    res = [] # list is mutable, hence self.res not needed
+    # recursive soln.
+    """ res = [] # list is mutable, hence self.res not needed
     def helper(node):
         if not node: return
         helper(node.left)
         res.append(node.val)
         helper(node.right)
     helper(root)
+    return res """
+    
+    # iterative soln.
+    # add all left childs to stack, pop last one, add to result and then change node to its right child
+    res, stack = [], []
+    while root or stack:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        res.append(root.val)
+        root = root.right
     return res
